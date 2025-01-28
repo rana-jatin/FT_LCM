523fee713fb2730efd0ca961a55d602db1847e3c
rana-jatin <163754674+rana-jatin@users.noreply.github.com>
2025-01-28 19:34:42 +0530

    Update Mkath.py

--- Mkath.py
+++ Mkath.py
@@ -46,7 +46,11 @@ st.chat_message("user").write(prompt)
 
     # Get Gemini response
 try:
-        response = model.generate_content(prompt)
+        response = model.generate_content(prompt ,safety_settings={
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+    })
         msg = response.text
 
         # Add assistant response to chat history
